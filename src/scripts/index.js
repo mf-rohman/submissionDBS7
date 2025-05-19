5326430050e776f3001e344b6cc653c9bd133126
mf-rohman <mf.rohman0101@gmail.com>
2025-05-20 02:03:12 +0700

    fix crash app when deploy- 11 eidt config.vite in order to auto generate sw.js and manifets.json

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -27,17 +27,7 @@ document.addEventListener("DOMContentLoaded", async () => {
   nameBrand();
   if ("serviceWorker" in navigator) {
     window.addEventListener("load", () => {
-      navigator.serviceWorker
-        .register("/service-worker.js")
-        .then((registration) => {
-          console.log(
-            "Service Worker registered with scope:",
-            registration.scope
-          );
-        })
-        .catch((error) => {
-          console.error("Service Worker registration failed:", error);
-        });
+      navigator.serviceWorker.register("/sw.js");
     });
   }
 });
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import { resolve } from "path";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: "/",
   root: resolve(__dirname, "src"),
@@ -16,4 +16,54 @@ export default defineConfig({
       "@": resolve(__dirname, "src"),
     },
   },
+  plugins: [
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: [
+        "favicon.png",
+        "images/logo.png",
+        "images/kana-logo192.png",
+        "images/kana-logo512.png",
+      ],
+      manifest: {
+        name: "Elite Global",
+        short_name: "Elite",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#000000",
+        icons: [
+          {
+            src: "images/kana-logo192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "images/kana-logo512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,png,svg,jpg,jpeg,json}"],
+        runtimeCaching: [
+          {
+            urlPattern: ({ request }) => request.destination === "document",
+            handler: "NetworkFirst",
+            options: {
+              cacheName: "pages",
+            },
+          },
+          {
+            urlPattern: ({ request }) => request.destination === "image",
+            handler: "CacheFirst",
+            options: {
+              cacheName: "images",
+            },
+          },
+        ],
+      },
+    }),
+  ],
 });
